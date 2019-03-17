fb469052303ec8813bab687fe13e9d808f117fea
Kishalaya Maity <maitykishalaya41@gmail.com>
2019-03-18 00:16:50 +0530

    First Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private float vel = 5;
+	public GameObject Ball;
+	Rigidbody2D Rb;
+
+	void Awake() {
+		Rb = GetComponent <Rigidbody2D> ();
+	}
+
+	public void UpwardVelocity () {
+		Rb.velocity += Vector2.up * vel * Time.deltaTime;
+	}
+
+	public void DownwardVelocity () {
+		Rb.velocity += Vector2.down * vel * Time.deltaTime;
+	}
+
+}
+
+
